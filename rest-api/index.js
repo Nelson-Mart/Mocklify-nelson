d4ac95f03bc9b925a6bed73cb33e72a41a8b33bd
Nelson-Mart <nelsonmartinel2@gmail.com>
2026-01-03 23:02:55 +0100

    git init

---
+++ rest-api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const productsRoutes = require("./routes/products.routes");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/products", productsRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
